2b79fc819ed9e0ad43fae28ffa2bb170208e3af9
Michael Hulet <bestraysfan@me.com>
2015-06-30 02:41:08 -0500

    Fixed spacing that my text editor didn't tell me about

--- Nothing But Net/robot.h
+++ Nothing But Net/robot.h
@@ -14,14 +14,14 @@
 typedef enum _MHMotorPower{
   //To stop is to not move. Duh.
   MHMotorPowerStop = 0,
-	//If you wanna stall the motors, it's safe to put it at a power of 30, but that's about the lowest it's safe to go
-	MHMotorPowerStall = 30,
-	//31 is about the minimum power you can assign to a motor before it begins to twitch
-	MHMotorPowerMin = 31,
-	//While a true half is 63.5, we're defining integer constants here, so we'll give it the high end
-	MHMotorPowerHalf = 64,
-	//For some reason, the maximum possible power for a VEX motor is 127. Don't ask me why.
-	MHMotorPowerMax = 127
+  //If you wanna stall the motors, it's safe to put it at a power of 30, but that's about the lowest it's safe to go
+  MHMotorPowerStall = 30,
+  //31 is about the minimum power you can assign to a motor before it begins to twitch
+  MHMotorPowerMin = 31,
+  //While a true half is 63.5, we're defining integer constants here, so we'll give it the high end
+  MHMotorPowerHalf = 64,
+  //For some reason, the maximum possible power for a VEX motor is 127. Don't ask me why.
+  MHMotorPowerMax = 127
 }MHMotorPower;
 //Enumeration to define robot drive sides
 typedef enum _MHDriveSide{
